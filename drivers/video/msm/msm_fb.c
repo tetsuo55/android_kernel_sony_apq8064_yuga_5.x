ee6100a76ece91869607a8cf7a7c4c77ff6f1347
Paul Reioux <reioux@gmail.com>
2013-08-16 03:27:42 -0500

    msm_kcal_ctrl: initial code for generic MSM LCD temperature calibrations
    this should be compatible with all MDP devices from Qualcomm
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    MSM KCAL: add sRGB LUT for better color space profiling
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    MSM KCAL: add lut edit capability
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    MSM KCAL: add checksum and data integrity checks
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    MSM KCAL: add reset lut capability
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    msm_kcal: add late resume drivers to autoload the lut
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    msm_kcal: update early suspend to resume last
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    drivers/video/msm: restore update_lcdc_lut capabilities for MDP devices
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Revert "video: msm: Initialize HistLUT with last saved LUT"
    
    This reverts commit 596d017f93a9b5d479a1edc18a0d21876e0d63a6.
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    
    Revert "MSM KCAL: add checksum and data integrity checks"
    
    This reverts commit 9f9b9438aebc66a48f0c6508e07adb5d8ac27b78.
    
    Conflicts:
    	arch/arm/mach-msm/Makefile
    	arch/arm/mach-msm/asustek/flo/board-flo-display.c
    	arch/arm/mach-msm/include/mach/board.h
    	drivers/video/msm/mdp.c
    	drivers/video/msm/msm_fb.c
    
    Conflicts:
    	arch/arm/mach-msm/Makefile

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -1686,6 +1686,11 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 	mfd->index, fbi->var.xres, fbi->var.yres, fbi->var.bits_per_pixel,
 							fbi->fix.smem_len);
 
+#ifdef CONFIG_UPDATE_LCDC_LUT
+	if (msm_fb_pdata->update_lcdc_lut)
+		msm_fb_pdata->update_lcdc_lut();
+#endif
+
 	ret = 0;
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
